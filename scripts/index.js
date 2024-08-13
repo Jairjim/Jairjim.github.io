1295ce6b0619725b04c0fb2f7a5297bac96ee28a
Jair Josué Jimarez García <jairjimarez2001@gmail.com>
2024-08-12 20:06:50 -0600

    Sections modified

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,33 @@
-var navbar  = document.getElementById("navbar")
-var landing = document.getElementById("home")
-var aboutme = document.getElementById("about-me")
+var navbar  = document.getElementById("navbar");
+var aboutme = document.getElementById("about-me");
+const windowHeight  = document.documentElement.clientHeight;
 
 
 window.onscroll = function(){
     var topWindow     = document.documentElement.scrollTop;
-    var windowHeight  = document.documentElement.clientHeight;
     var position      = topWindow / windowHeight;
-    navbar.style.setProperty('opacity', position)
+    navbar.style.setProperty('opacity', position);
+
+}
+
+
+
+function scrollWindow(element){
+    var displacement = 0;
+    
+    if(element == "landing"){
+        displacement = 0;
+    }else if(element == "aboutme"){
+        displacement = windowHeight;
+    }else if(element == "projects"){
+        displacement = windowHeight * 2;
+    }else if(element == "achievements"){
+        displacement = windowHeight * 3;
+    }
+
+    window.scroll({
+        top: displacement,
+        behavior: "smooth",
+    });
 
 }
